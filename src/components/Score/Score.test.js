8e597616d6cf900f08d79eb1692a7517185a5f45
Khanh <hkhanh.ng@gmail.com>
2019-09-26 23:03:05 +0300

    Change Score name; jsconfig instead .env for baseURL

--- src/components/Score/Score.test.js
+++ src/components/Score/Score.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Scoresheet from './Scoresheet';
+import Scoresheet from './Score';
 
 describe('Scoresheet component test', () => {
    it('renders without crashing', () => {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Scoresheet from './components/Scoresheet';
+import Score from './components/Score';
 import Start from './components/Start';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -15,7 +15,7 @@ ReactDOM.render(
         <ReactNotification />
         <Router>
             <Switch>
-                <Route path="/game/:id" component={Scoresheet} />
+                <Route path="/game/:id" component={Score} />
                 <Route path="/" exact component={Start} />
                 <Route render={() => <h3>404</h3>} />
             </Switch>
