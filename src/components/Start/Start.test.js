fb0fbf61bc277c832a36cf120d394b2e8e3f5111
Khanh <hkhanh.ng@gmail.com>
2019-10-02 00:56:36 +0300

    rm duplicate file with case sensitive

--- src/components/Start/Start.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Start from './Start';
-
-describe('Scoresheet component test', () => {
-    it('renders without crashing', () => {
-        shallow(<Start />);
-    });
-});
